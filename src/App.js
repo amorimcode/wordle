6e56319ec4163026da1fdc02ebd7c78601be50f9
Bruno Amorim <bruno.amorim032@gmail.com>
2022-04-15 21:31:44 -0300

    backspace working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import './App.scss';
+import "./App.scss";
+import { useEffect, useState } from "react";
 
-import Keyboard from './components/Keyboard'
-import GameBoard from './components/GameBoard';
+import Keyboard from "./components/Keyboard";
+import GameBoard from "./components/GameBoard";
 
-const handleInsertLetter = (letter) => {
-  
+function lettersOnly(event) {
+  var charCode = event.keyCode;
+
+  if (
+    (charCode > 64 && charCode < 91) ||
+    (charCode > 96 && charCode < 123) ||
+    charCode == 8
+  )
+    return true;
+  else return false;
 }
 
 function App() {
+  const [guessWord, setGuessWord] = useState([]);
+  const [word, setWord] = useState("");
+
+  document.addEventListener("keydown", (event) => {
+    
+    if (event.key === 'Backspace') {
+      const updateGuessWordArray = guessWord.slice(0, -1);;
+
+      console.log(guessWord)
+      setGuessWord(updateGuessWordArray);
+    }
+
+    if (lettersOnly(event) && event.key.length === 1) {
+      
+      if (guessWord.length < 5) {
+        const updateGuessWordArray = [...guessWord, event.key];
+
+        setGuessWord(updateGuessWordArray);
+      }
+
+      console.log(guessWord);
+    }
+  });
 
   return (
     <>
       <h1>Wordle</h1>
-      <GameBoard />
+      <GameBoard guessWord={guessWord} />
       <Keyboard />
     </>
-
   );
 }
 
--- src/components/GameBoard/index.js
+++ src/components/GameBoard/index.js
@@ -1,43 +1,15 @@
-import { useEffect, useState } from "react";
 
 
-export default function GameBoard() {
-  const [guessWord, setGuessWord] = useState('');
 
-
-  function insertLetter(letter) {
-    var div = document.getElementById("row-1");
-    var letters = div.getElementsByClassName("letter-box");
-    for (var i = 0; letters.length; i++) {
-
-      if (letters[i].innerHTML === '') {
-        letters[i].innerHTML = letter
-        setGuessWord(guessWord + letter)
-        return
-      }
-
-      console.log(guessWord)
-    }
-
-  }
-
-  useEffect(() => {
-    document.addEventListener("keydown", (e) => {
-      let pressedKey = String(e.key);
-      let found = pressedKey.match(/[a-z]/gi);
-      insertLetter(found);
-    });
-
-  });
-
+export default function GameBoard({guessWord}) {
   return (
     <div id="game-board">
       <div className="letter-row" id="row-1">
-        <div className="letter-box"></div>
-        <div className="letter-box"></div>
-        <div className="letter-box"></div>
-        <div className="letter-box"></div>
-        <div className="letter-box"></div>
+        <div className="letter-box">{guessWord[0]}</div>
+        <div className="letter-box">{guessWord[1]}</div>
+        <div className="letter-box">{guessWord[2]}</div>
+        <div className="letter-box">{guessWord[3]}</div>
+        <div className="letter-box">{guessWord[4]}</div>
       </div>
       <div className="letter-row" id="row-2">
         <div className="letter-box"></div>
